ac0dd85d1c63bf0c3a0f2ea2bcad2c99e67fd2b8
Teigen Leonard <teigenl@gmail.com>
2018-04-04 12:10:31 -0500

    added LoginController

---
+++ public/scripts/client.js
@@ -0,0 +1,10 @@
+var loginApp = angular.module("LoginApp", []);
+
+loginApp.controller("LoginController", function () {
+    var login = this;
+
+    console.log('hello in tharr');
+
+    list.hello = "Hello World!";
+
+});
