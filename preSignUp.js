42bac23b28860da16bb4e8a7c9ccb28f063d73d8
Teigen Leonard <teigenl@gmail.com>
2018-04-26 17:24:05 -0500

    widened permissions in IAM but preSignUp.js still not creating user error:<Missing Authentication Token>

--- preSignUp.js
+++ preSignUp.js
@@ -3,14 +3,13 @@
 module.exports.handler = (event, context, callback) => {
     const response = {
         statusCode: 200,
-        autoConfirmUser: true,
+        // autoConfirmUser: true,
         headers: {
             "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
             "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
         },
         body: JSON.stringify({
-            message: 'Howdy from preSignUp.js',
-            input: event
+            "message": 'Howdy from preSignUp.js'
         }),
     };
 
