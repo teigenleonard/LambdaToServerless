2d26ab6f96bbe1c74e3f21377f77742a1ce74888
Teigen Leonard <teigenl@gmail.com>
2018-05-03 14:48:47 -0500

    add cognito resource and comment out preSignUp lambda function

--- handler.js
+++ handler.js
@@ -1,21 +1,21 @@
 'use strict';
 
-  //handler.js is triggered by handler.hello and HelloWorld-charter-test-hello on AWS
+//handler.js is triggered by handler.hello and HelloWorld-charter-test-hello on AWS
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    headers: {
-        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-    },
-    body: JSON.stringify({
-      message: 'Howdy from handler.js',
-      input: event
-    }),
-  };
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+        },
+        body: JSON.stringify({
+            message: 'Howdy from handler.js',
+            input: event
+        }),
+    };
 
-  callback(null, response);
+    callback(null, response);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
--- preSignUp.js
+++ preSignUp.js
@@ -3,7 +3,7 @@
 module.exports.handler = (event, context, callback) => {
     const response = {
         statusCode: 200,
-        // autoConfirmUser: true,
+        autoConfirmUser: true,
         headers: {
             "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
             "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
@@ -15,6 +15,4 @@ module.exports.handler = (event, context, callback) => {
 
     callback(null, response);
 
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
