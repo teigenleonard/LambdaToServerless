c728ec55dd62021763672a3aeb68d47a9d817f49
Teigen Leonard <teigenl@gmail.com>
2018-06-05 14:57:38 -0500

    set configuration add body-parser

--- server/app.js
+++ server/app.js
@@ -1,6 +1,29 @@
-var express = require( 'express' );
-var path = require( 'path' );
-var app = express();
+const express = require( 'express' );
+const path = require( 'path' );
+const bodyParser = require( 'body-parser' );
+const Amplify = require( 'aws-amplify' );
+const config = require( './config' );
+
+const app = express();
+
+Amplify.configure({
+    Auth: {
+        mandatorySignIn: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    },
+    API: {
+        endpoints: [
+            {
+                name: 'testApiCall',
+                endpoint: config.apiGateway.URL,
+                region: config.apiGateway.REGION
+            }
+        ]
+    }
+});
 
 app.set( 'port', ( process.env.PORT || 5000 ));
 
